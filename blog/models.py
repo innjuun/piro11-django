f22dbe079ff2d12e50047e0ceb9acf1d6ad7a900
Hwang-Injun <sroo2315@gmail.com>
2019-07-17 22:43:35 +0900

    blog.models.Post model made/migration/submit admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100,
+                             choices=(
+                                 ('제목1', '제목1 레이블'),  # 저장될 값과 UI에 보여질 레이블
+                                 ('제목2', '제목3 레이블'),
+                                 ('제목3', '제목3 레이블'),
+
+                             ),
+                             verbose_name="제목",
+                             help_text='포스팅 제목을 입력해주세요',
+                             )
+    content = models.TextField(verbose_name="내용")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
