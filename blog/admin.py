e9611d74436532deb02db2ace77590e53603b149
Hwang-Injun <sroo2315@gmail.com>
2019-07-18 16:58:31 +0900

    blog.modles.Post 모델에 status 필드 추가, 이를 처리하는 custom Admin Action 구현

--- blog/admin.py
+++ blog/admin.py
@@ -2,13 +2,22 @@ from django.contrib import admin
 from django.utils.safestring import mark_safe
 
 from .models import Post
+
+
 # Register your models here.
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'content_size', 'created_at', 'updated_at' ]
+    list_display = ['id', 'title', 'content_size',
+                    'status', 'created_at', 'updated_at']
+    actions = ['make_published']
 
     def content_size(self, post):
         return mark_safe(("<strong>{}</strong>글자".format(len(post.content))))
+
+    def make_published(self, request, queryset):
+        updated_count =queryset.update(status='p')
+        self.message_user(request, '{}건의 포스팅을 published 상태로 변경'.format(updated_count))
+    make_published.short_description = '지정 포스팅을 published상태로 변경'
     content_size.short_description = "글자수"
-    #content_size.allow_tags = True
-#admin.site.register(Post, PostAdmin)
+    # content_size.allow_tags = True
+# admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.forms import ValidationError
 import re
 
+
 # Create your models here.
 def lnglat_validator(value):
     if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
@@ -9,6 +10,11 @@ def lnglat_validator(value):
 
 
 class Post(models.Model):
+    STATUS_CHOICES = (
+        ('d', 'Draft'),
+        ('p', 'Published'),
+        ('w', 'Withdrawn'),
+    )
     author = models.CharField(max_length=20)
     title = models.CharField(max_length=100,
                              choices=(
@@ -21,9 +27,9 @@ class Post(models.Model):
                              help_text='포스팅 제목을 입력해주세요',
                              )
     content = models.TextField(verbose_name="내용")
-    tags = models.CharField(max_length=100, blank=True,)
+    tags = models.CharField(max_length=100, blank=True, )
     lnglat = models.CharField(max_length=50, validators=[lnglat_validator],
-                              help_text='경도,위도 포맷으로 입력', blank=True,)
-
+                              help_text='경도,위도 포맷으로 입력', blank=True, )
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
