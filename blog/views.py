214327c8fdb37fdc951f314bb87babbbafa5fcfd
Hwang-Injun <sroo2315@gmail.com>
2019-07-19 23:41:10 +0900

    blog.views.port_list 뷰에 queryset 검색 구현

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Post
+
+
 def post_list(request):
     qs = Post.objects.all()
-
-    return render(request, 'blog/post_list.html', {'post_list': qs,})
+    q = request.GET.get('q', '')
+    if q:
+        qs = qs.filter(title__icontains=q)
+    return render(request, 'blog/post_list.html', {'post_list': qs, 'q': q, })
