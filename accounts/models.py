5949dbe4f85155e1dcac327a2c70bbbe671f6617
Hwang-Injun <sroo2315@gmail.com>
2019-07-21 16:20:31 +0900

    settings.AUTH_USER_MODEL 올바른 지정

--- accounts/models.py
+++ accounts/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)  # FIXME
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # FIXME
     phone_number = models.CharField(max_length=20)
     address = models.CharField(max_length=50)
