34c8a9a21d64913ddf875fe3c1386e78f4deb847
Hwang-Injun <sroo2315@gmail.com>
2019-08-02 17:59:35 +0900

    filesystem static finder 설정 및 static 샘플 추가

--- practice/settings.py
+++ practice/settings.py
@@ -132,7 +132,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'practice', 'static'),
+]
 
 STATIC_ROOT = join(BASE_DIR, '..', ''
                                    ''
